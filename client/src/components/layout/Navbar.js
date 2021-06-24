4a7faa0c6894d0eebc07620f4cf2dacb217ac313
Al-amin Yusuf <alaminyusuf131@gmail.com>
2021-06-24 12:30:03 +0100

    added auth

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
-import styled from 'styled-components'
-
+import { useSelector } from 'react-redux'
 const Navbar = () => {
+  const { isAuthenticated } = useSelector(state => state.user)
   const location = useLocation()
   const isLogin = location.pathname === '/login'
   const isSignUp = location.pathname === '/signup'
@@ -14,27 +14,25 @@ const Navbar = () => {
       </Link>
       {isLogin || isSignUp ? null : (
         <>
-          <Link to="/chat" className="ui item">
-            Chat
-          </Link>
           <Link to="/profile" className="ui item">
             Profile
           </Link>
         </>
       )}
-
-      <div className="right item">
-        {isLogin ? null : (
-          <Link to="/login" className="ui item secondary">
-            Log In
-          </Link>
-        )}
-        {isSignUp ? null : (
-          <Link to="/signup" className="ui item secondary">
-            Sign Up
-          </Link>
-        )}
-      </div>
+      {!isAuthenticated ? (
+        <div className="right item">
+          {isLogin ? null : (
+            <Link to="/login" className="ui item secondary">
+              Log In
+            </Link>
+          )}
+          {isSignUp ? null : (
+            <Link to="/signup" className="ui item secondary">
+              Sign Up
+            </Link>
+          )}
+        </div>
+      ) : null}
     </div>
   )
 }
