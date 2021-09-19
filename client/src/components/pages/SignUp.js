24894b88b8c02dfb8970f2f465a8a046407d22eb
Al-amin Yusuf <alaminyusuf131@gmail.com>
2021-09-19 17:54:44 +0100

    transitioned from formik to using states

--- client/src/components/pages/SignUp.js
+++ client/src/components/pages/SignUp.js
@@ -1,35 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Redirect } from 'react-router'
+import { signUpUser } from '../../store/ducks/user'
 
 const SignUp = () => {
+  const [fields, setFields] = useState({
+    name: '',
+    email: '',
+    password: '',
+  })
+  const dispatch = useDispatch()
+  const { isAuthenticated, error } = useSelector(state => state.user)
+  const emailFieldError = error && error.field === 'email'
+  const nameFieldError = error && error.field === 'name'
+  const passwordFieldError = error && error.field === 'password'
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    await dispatch(signUpUser(fields))
+  }
+
+  const handleChange = e => {
+    const value = e.target.value
+    setFields({
+      ...fields,
+      [e.target.name]: value,
+    })
+  }
   return (
     <div style={{ margin: '2em 0' }}>
-      <Formik
-        initialValues={{ name: '', email: '', password: '' }}
-        onSubmit={async ({ email, name, password }) => {
-          console.log({ name, email, password })
-          handleSubmit({
-            email,
-            name,
-            password,
-          })
-        }}
-      >
-        <Form className="ui form">
+      {isAuthenticated ? (
+        <Redirect to="/" />
+      ) : (
+        <form className="ui form" onSubmit={handleSubmit}>
           <div className="field">
             <label htmlFor="name">Name</label>
-            <input type="text" name="name" placeholder="Name" />
+            {nameFieldError && <div>{error.info} </div>}
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              value={fields.name}
+              onChange={handleChange}
+            />
           </div>
           <div className="field">
             <label htmlFor="email">Email</label>
-            <input type="email" name="email" placeholder="Email" />
+            {emailFieldError && <div>{error.info} </div>}
+            <input
+              type="email"
+              name="email"
+              onChange={handleChange}
+              value={fields.email}
+              placeholder="Email"
+            />
           </div>
           <div className="field">
+            {passwordFieldError && <div>{error.info} </div>}
             <label htmlFor="password">Password</label>
-            <input type="password" name="password" placeholder="Password" />
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={fields.password}
+              onChange={handleChange}
+            />
           </div>
           <input className="ui secondary button" type="submit" value="Submit" />
-        </Form>
-      </Formik>
+        </form>
+      )}
     </div>
   )
 }
