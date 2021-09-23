26a29f9607d4122cfd277c8f8a304a845bbd9830
Al-amin Yusuf <alaminyusuf131@gmail.com>
2021-09-23 21:04:22 +0100

    removed unwated packages

--- client/src/style/theme.js
+++
@@ -1,9 +0,0 @@
-export const theme = {
-  primaryButton: '#5f9ea0',
-  primaryLight: '#effffa',
-  primaryDark: '#343078',
-  mobile: '567px',
-  headerFont: '25px',
-  secondaryButton: '#00ffff',
-  shadow: 'rgba(0, 0, 0, 0.3) 0.1rem 0.2rem 0.5rem',
-}
